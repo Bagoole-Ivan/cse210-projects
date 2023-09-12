ed1c995987153cce816cd5a5e709fbc7b11a9b90
Bagoole ivan <ivanbagoole2001@gmail.com>
2023-09-12 20:15:53 +0300

    exercise 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        string firstName = "What is your first name? ";
+        Console.Write(firstName);
+        string name1 = Console.ReadLine();
+        string sirName = "What is your sir name? ";
+        Console.Write(sirName);
+        string name2 = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {name1}, {name2}  {name1}");
+
     }
 }
