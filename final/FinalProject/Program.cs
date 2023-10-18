8fd1368b288c55a4dd88b0128b47b2600ee57e07
Bagoole ivan <ivanbagoole2001@gmail.com>
2023-10-18 20:06:46 +0300

    project

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program
+namespace simpleCalculator
 {
-    static void Main(string[] args)
+
+    /*Simple Calculator to calculate the perimeter of a Rectangle and a Triangle */
+
+    /* Abstraction */
+    abstract class Figure
     {
-        Console.WriteLine("Hello FinalProject World!");
+        abstract public Int32 Perimeter();
+    }
+
+    /* Inheriting from the Figure class */
+    class Rentangle : Figure
+    {
+        public Rentangle(Int32 w, Int32 h)
+        {
+            this.w = w;
+            this.h = h;
+        }
+
+        /* Encasulation */
+        public Int32 w { get; set; }
+        public Int32 h { get; set; }
+
+        /* Polymorphism */
+        public override Int32 Perimeter()
+        {
+            return 2 * w + 2 * h;
+        }
+    }
+
+    class Triangle : Figure
+    {
+        public Triangle(Int32 s1, Int32 s2, Int32 s3)
+        {
+            this.s1 = s1;
+            this.s2 = s2;
+            this.s3 = s3;
+        }
+
+        public Int32 s1 { get; set; }
+        public Int32 s2 { get; set; }
+        public Int32 s3 { get; set; }
+
+        public override Int32 Perimeter()
+        {
+            return s1 + s2 + s3;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure rentangle = new Rentangle(10, 23);
+            Figure triangle = new Triangle(23, 11, 9);
+
+            Int32 rentanglePerimeter = rentangle.Perimeter();
+            Int32 trianglePerimeter = triangle.Perimeter();
+
+            Console.WriteLine("Rentangle perimeter: " + rentanglePerimeter);
+            Console.WriteLine("Triangle perimeter: " + trianglePerimeter);
+        }
     }
 }
