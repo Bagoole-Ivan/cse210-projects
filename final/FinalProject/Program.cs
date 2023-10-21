b5b58f4901c6847f2fcbe33c5a22550c8c5d1cc6
Bagoole ivan <ivanbagoole2001@gmail.com>
2023-10-21 17:23:05 +0300

    changed

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -18,6 +18,7 @@ namespace simpleCalculator
     abstract class Figure
     {
         abstract public Int32 Perimeter();
+        abstract public Int32 Area();
     }
 
     /* Inheriting from the Figure class */
@@ -38,9 +39,13 @@ namespace simpleCalculator
         {
             return 2 * w + 2 * h;
         }
-    }
 
-    class Square : Figure
+        public override int Area()
+        {
+            return w * h;
+        }
+    }
+        class Square : Figure
     {
         public Square(Int32 s)
         {
@@ -54,6 +59,11 @@ namespace simpleCalculator
         public override Int32 Perimeter()
         {
             return s * 4;
+        }
+
+        public override int Area()
+        {
+            return s * s;
         }
     }
 
@@ -82,49 +92,59 @@ namespace simpleCalculator
         /* Polymorphism */
         public override Int32 Perimeter()
         {
-            return s1 + s2 + s3 + s4 + s5 + s6 + s7;
+            return s1 + s2 +s3 + s4 + s5 + s6 + s7;
+        }
+
+        public override int Area()
+        {
+            throw new NotImplementedException();
         }
     }
 
     class Triangle : Figure
     {
-        public Triangle(Int32 s1, Int32 s2, Int32 s3)
+        public Triangle(Int32 s1, Int32 s2, Int32 b, Int32 h)
         {
             this.s1 = s1;
             this.s2 = s2;
-            this.s3 = s3;
+            this.b = b;
+            this.h = h;
         }
 
         public Int32 s1 { get; set; }
         public Int32 s2 { get; set; }
-        public Int32 s3 { get; set; }
+        public Int32 b { get; set; }
+        public Int32 h { get; set; }
 
         public override Int32 Perimeter()
         {
-            return s1 + s2 + s3;
+            return s1 + s2 + b;
+        }
+
+        public override int Area()
+        {
+            return (1 / 2) * b * h ;
         }
     }
 
     class Polygon : Figure
     {
-        public Polygon(Int32 s1, Int32 s2, Int32 s3, Int32 s4, Int32 s5)
+        /* This is a polygon with five equal sides */
+        public Polygon(Int32 s1)
         {
             this.s1 = s1;
-            this.s2 = s2;
-            this.s3 = s3;
-            this.s4 = s4;
-            this.s5 = s5;
         }
 
         public Int32 s1 { get; set; }
-        public Int32 s2 { get; set; }
-        public Int32 s3 { get; set; }
-        public Int32 s4 { get; set; }
-        public Int32 s5 { get; set; }
 
         public override Int32 Perimeter()
         {
-            return s1 + s2 + s3 + s4 + s5;
+            return s1 * 5;
+        }
+
+        public override int Area()
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -133,9 +153,9 @@ namespace simpleCalculator
         static void Main(string[] args)
         {
             Figure rectangle = new Rectangle(10, 23);
-            Figure triangle = new Triangle(23, 11, 9);
+            Figure triangle = new Triangle(23, 11, 9, 10);
             Figure square = new Square(4);
-            Figure polygon = new Polygon(5, 5, 5, 5, 5);
+            Figure polygon = new Polygon(5);
             Figure IrregularPentagon = new irregularPentagon(3, 6, 8, 9, 11, 13, 16);
 
             Int32 rectanglePerimeter = rectangle.Perimeter();
@@ -144,11 +164,24 @@ namespace simpleCalculator
             Int32 polygonPerimeter = polygon.Perimeter();
             Int32 IrregularPentagonPerimeter = IrregularPentagon.Perimeter();
 
+
+            Int32 rectangleArea = rectangle.Area();
+            Int32 triangleArea = triangle.Area();
+            Int32 squareArea = square.Area();
+            Int32 polygonArea = polygon.Area();
+            Int32 IrregularPentagonArea = IrregularPentagon.Area();
+
             Console.WriteLine("Rectangle perimeter: " + rectanglePerimeter);
             Console.WriteLine("Triangle perimeter: " + trianglePerimeter);
             Console.WriteLine("Square perimeter: " + squarePerimeter);
             Console.WriteLine("Polygon perimeter: " + polygonPerimeter);
             Console.WriteLine("irregularPentagon perimeter" + IrregularPentagonPerimeter);
+
+
+            Console.WriteLine("Rectangle area: " + rectangleArea);
+            Console.WriteLine("Triangle area: " + triangleArea);
+            Console.WriteLine("Square area: " + squareArea);
+
         }
     }
 }
